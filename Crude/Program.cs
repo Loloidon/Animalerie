157406cce65925dc8cdf877d26b352713e254082
Lolo <lolo.test@test.com>
2023-06-23 16:38:27 +0200

    Ajoutez des fichiers projet.

---
+++ Crude/Program.cs
@@ -0,0 +1,65 @@
+﻿using Crude;
+using System.Text;
+
+List<Dog> Dogs = new List<Dog>();
+Dogs.Add(new Dog("Archie",10,DogRace.Dalmatien));
+int input,age;
+bool done = false;
+string name;
+string result = "";
+do
+{
+    Console.WriteLine("Appuyer sur 0 pour sortir || 1 pour ajouter un chien || 2 pour afficher la liste || 3 pour enlever des éléments de la liste");
+    input = int.Parse(Console.ReadLine());
+    StringBuilder sb = new StringBuilder();
+
+    switch (input)
+    {
+
+        case 0:done=true;
+            Console.WriteLine("Aurevoir");
+            
+            break;
+
+        case 1:
+            Console.WriteLine("Entrez son nom,âge et race");
+            name = Console.ReadLine();
+            age = int.Parse(Console.ReadLine());
+
+
+
+
+
+            foreach (DogRace p in Enum.GetValues<DogRace>())
+            {
+                sb.AppendLine((int)p < 4 ? $"{(int)p + 1}  : {p}" : $"{(int)p + 1} : {p}");
+
+            }
+            Console.WriteLine(sb);
+            Console.WriteLine(result);
+
+
+            string stringType = Console.ReadLine();
+
+            int nbType = int.Parse(stringType);
+
+            //PokemonType type = Enum.Parse<PokemonType>(stringType);
+            DogRace race = (DogRace)nbType - 1;
+            Dogs.Add(new Dog(name, age, race));
+
+
+            break;
+        case 2:
+            foreach ( Dog dog in Dogs)
+            {
+                Console.WriteLine(dog);
+            }
+
+            break;
+        case 3:
+          
+
+            break;
+    } }while (!done);
+
+
---
+++ Crude/Structure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Crude;
+namespace Crude
+{
+   
+        public struct Dog {
+            public string name;
+            public int age;
+            public DogRace Race;
+
+            public Dog(string name, int age, DogRace race)
+            {
+                this.name = name;
+                this.age = age;
+                Race = race;
+            }
+        public override string ToString()
+        {
+            return $"_______________________________\n" +
+                   $"Name : {name}\n" +
+                   $"Age : {age}\n" +
+                   $"Race : {Race}\n" +
+               
+                   $"_______________________________\n";
+        }
+    }
+
+
+ 
+
+        public enum DogRace {
+        Bordercollie,
+        Dalmatien,
+        Tekkel,
+        Chihuaha,
+    }
+
+    }
+
